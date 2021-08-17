c06034a687ca9d3439216ae22732e58f9d8cdfe3
kat witch <kat@kittywit.ch>
2021-08-17 22:51:10 +0100

    weh

--- lib/host-import.nix
+++ lib/host-import.nix
@@ -1,9 +1,9 @@
-{ lib }: { hostName, userName, profiles, root }: with lib; filter builtins.pathExists [
+{ lib }: { hostName, profiles, root }: with lib; filter builtins.pathExists [
   (root + "/depot/hosts/${hostName}/nixos.nix")
   (root + "/depot/trusted/hosts/${hostName}/nixos.nix")
 ] ++ (if builtins.isAttrs profiles.base then profiles.base.imports
 else singleton profiles.base) ++ singleton {
-  home-manager.users.${userName} = {
+  home-manager.users.kat = {
     imports = filter builtins.pathExists [
       (root + "/depot/hosts/${hostName}/home.nix")
       (root + "/depot/trusted/hosts/${hostName}/home.nix")
