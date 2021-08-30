3872f4f23c60b2a398ed074d4db60c6344bdd500
kat witch <kat@kittywit.ch>
2021-08-30 19:42:37 +0100

    swaylock gift

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,12 +1,14 @@
 {
   network = ./network.nix;
   firewall = ./firewall.nix;
+  swaylock = ./swaylock.nix;
 
   __functionArgs = { };
   __functor = self: { ... }: {
     imports = with self; [
       network
       firewall
+      swaylock
     ];
   };
 }
