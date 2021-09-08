7869e9c9e3e6b3586a836ae4434e4c45b910e4d5
kat witch <kat@kittywit.ch>
2021-09-09 00:56:22 +0100

    inhoused modules into nixfiles again

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,16 +1,10 @@
 {
-  network = ./network.nix;
-  firewall = ./firewall.nix;
   fusionpbx = ./fusionpbx.nix;
-  nftables = ./nftables.nix;
 
   __functionArgs = { };
   __functor = self: { ... }: {
     imports = with self; [
-      network
-      firewall
       fusionpbx
-      nftables
     ];
   };
 }
