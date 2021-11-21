825d770e704d063f4f9a6850e2f19ea8851f8cb0
kat witch <kat@kittywit.ch>
2021-11-21 20:19:44 +0000

    Moving to flakes, need not to pull in nixpkgs/lib

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,24 @@
 {
-  nixlib = import <nixpkgs/lib>;
+  foldr = op: nul: list:
+    let
+      len = builtins.length list;
+      fold' = n:
+        if n == len
+        then nul
+        else op (builtins.elemAt list n) (fold' (n + 1));
+    in fold' 0;
+  composeExtensions =
+    f: g: final: prev:
+      let fApplied = f final prev;
+          prev' = prev // fApplied;
+      in fApplied // g final prev';
+  composeManyExtensions =
+    foldr (x: y: composeExtensions x y) (final: prev: {});
+
   pkgs = import ./pkgs.nix;
   lib = import ./lib.nix;
 
-  __functor = fp: with fp; nixlib.composeManyExtensions [
+  __functor = fp: with fp; composeManyExtensions [
     lib
     pkgs
   ];
