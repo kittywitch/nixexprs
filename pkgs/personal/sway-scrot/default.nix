8f161a830e79564592667ab165580be89aeff300
kat witch <kat@kittywit.ch>
2022-01-04 19:45:30 +0000

    X11 dependencies

--- pkgs/personal/sway-scrot/default.nix
+++ pkgs/personal/sway-scrot/default.nix
@@ -1,5 +1,5 @@
 { wrapShellScriptBin, pkgs }:
 
 wrapShellScriptBin "sway-scrot" ./sway-scrot.sh {
-  depsRuntimePath = with pkgs; [ coreutils wl-clipboard slurp grim sway jq libnotify ];
+  depsRuntimePath = with pkgs; [ coreutils wl-clipboard slurp grim sway jq libnotify xdotool maim xclip ];
 }
